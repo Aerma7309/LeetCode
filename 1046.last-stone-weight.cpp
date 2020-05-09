bcf53e16ff751b4a5de984358443bf8bbf5d8c59
Abhimanyu Maurya <aerma7309@gmail.com>
2020-05-09 16:23:51 +0530

    1046.last-stone-weight.cpp

---
+++ 1046.last-stone-weight.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=1046 lang=cpp
+ *
+ * [1046] Last Stone Weight
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int a, b;
+    int lastStoneWeight(vector<int> &stones)
+    {
+        priority_queue<int> q;
+        for (auto &i : stones)
+            q.push(i);
+        while (q.size() > 1)
+        {
+            a = q.top();
+            q.pop();
+            b = q.top();
+            q.pop();
+            if ((a - b) > 0)
+                q.push(a - b);
+        }
+
+        if (q.empty())
+            return 0;
+        return q.top();
+    }
+};
+// @lc code=end
+
