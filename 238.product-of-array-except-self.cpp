4311f9d45038f0ec33504fb7b6fc2168ede4115b
Abhimanyu Maurya <aerma7309@gmail.com>
2020-05-09 16:33:34 +0530

    238.product-of-array-except-self.cpp

---
+++ 238.product-of-array-except-self.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=238 lang=cpp
+ *
+ * [238] Product of Array Except Self
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int> &nums)
+    {
+        int p = 1, cz = 0;
+        for (auto i : nums)
+        {
+            if (i)
+                p *= i;
+            else
+                cz++;
+        }
+        vector<int> v(nums.size());
+        if (cz > 1)
+            return v;
+        else if (cz == 1)
+        {
+            int i = find(nums.begin(), nums.end(), 0) - nums.begin();
+            v[i] = p;
+        }
+        else
+        {
+            for (int i = 0; i < nums.size(); i++)
+                v[i] = p / nums[i];
+        }
+        return v;
+    }
+};
+// @lc code=end
+
