f3bd490b5a85324a4bd61658afe2e3775a7aa841
Abhimanyu Maurya <aerma7309@gmail.com>
2020-05-10 15:24:00 +0530

    448.find-all-numbers-disappeared-in-an-array.cpp

---
+++ 448.find-all-numbers-disappeared-in-an-array.cpp
@@ -0,0 +1,30 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem448.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode id=448 lang=cpp
+ *
+ * [448] Find All Numbers Disappeared in an Array
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    vector<int> findDisappearedNumbers(vector<int> &nums)
+    {
+        vector<int> res;
+        for (int i = 0; i < nums.size(); i++)
+            nums[abs(nums[i]) - 1] = -1 * abs(nums[abs(nums[i]) - 1]);
+        for (int i = 0; i < nums.size(); i++)
+            if (nums[i] > 0)
+                res.push_back(i + 1);
+        return res;
+    }
+};
+// @lc code=end
