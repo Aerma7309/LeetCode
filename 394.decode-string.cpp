7968ab5455435e6a0e6b14918da5e14020311ab0
Abhimanyu Maurya <aerma7309@gmail.com>
2020-05-25 19:44:26 +0530

    394.decode-string.cpp

--- 394.decode-string.cpp
+++ 394.decode-string.cpp
@@ -8,33 +8,62 @@
 class Solution
 {
 public:
-    // int i = 0;
-    string decodeString(string &s, int &i)
-    {
-        string res = "";
-        while (i < s.size() and s[i] != ']')
-        {
-            if (!isdigit(s[i]))
-                res += s[i++];
-            else
-            {
-                int num = 0;
-                while (i < s.size() and isdigit(s[i]))
-                    num = num * 10 + (s[i++] - '0');
-                i++; //[
-                string subRes = decodeString(s,i);
-                i++; //]
-                while (num--)
-                    res += subRes;
-            }
-        }
-        return res;
-    }
+    // string decodeString(string &s, int &i)
+    // {
+    //     string Ans="";
+    //     while (i < s.size() and s[i] != ']')
+    //     {
+    //         if (isdigit(s[i]))
+    //         {
+    //             int num = 0;
+    //             while (i < s.size() and isdigit(s[i]))
+    //                 num = num * 10 + (s[i++] - '0');
+    //             i++;//[
+    //             string chhotaAns = decodeString(s,i);
+    //             i++;//]
+    //             while (num--) Ans+=chhotaAns;
+    //         }
+    //         else Ans+=s[i++];
+    //     }
+    //     return Ans;
+    // }
+    // string decodeString(string &s)
+    // {
+    //     int i = 0;
+    //     return decodeString(s, i);
+    // }
 
     string decodeString(string &s)
     {
-        int i = 0;
-        return decodeString(s, i);
+        stack<string> stkStr;
+        stack<int> stkInt;
+        string Ans = "";
+        int num = 0;
+        for (auto &i : s)
+        {
+            if (isdigit(i))
+                num = num * 10 + (i - '0');
+            else if (i == '[')
+            {
+                stkStr.push(Ans);
+                stkInt.push(num);
+                Ans = "";
+                num = 0;
+            }
+            else if (i == ']')
+            {
+                string res = stkStr.top();
+                int num = stkInt.top();
+                stkStr.pop();
+                stkInt.pop();
+                while (num--)
+                    res += Ans;
+                Ans = res;
+            }
+            else
+                Ans.push_back(i);
+        }
+        return Ans;
     }
 };
 // @lc code=end
