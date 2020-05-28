f42d99236e5ced5f0da1decedfdee6787ed51974
Abhimanyu Maurya <aerma7309@gmail.com>
2020-05-28 22:56:59 +0530

    344.reverse-string.cpp

--- 344.reverse-string.cpp
+++ 344.reverse-string.cpp
@@ -8,10 +8,23 @@
 class Solution
 {
 public:
-    void reverseString(vector<char> &s)
+    // void reverseString(vector<char> &s)
+    // {
+    //     for (int i = 0, j = s.size() - 1; i < j; i++, j--)
+    //         swap(s[i], s[j]);
+    // }
+
+    void reverse_helper(vector<char> &s, int l, int r)
     {
-        for (int i = 0, j = s.size() - 1; i < j; i++, j--)
-            swap(s[i], s[j]);
+        if (l >= r)
+            return;
+        swap(s[l], s[r]);
+        reverse_helper(s, l + 1, r - 1);
+    }
+
+    void reverseString(vector<char> &s, int i = 0)
+    {
+        reverse_helper(s, 0, s.size()-1);
     }
 };
 // @lc code=end
