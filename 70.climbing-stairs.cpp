e3f842e1dbff7e4a81d68bb0f4eb20b71526cbdd
Abhimanyu Maurya <aerma7309@gmail.com>
2020-05-29 08:04:39 +0530

    70.climbing-stairs.cpp

---
+++ 70.climbing-stairs.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=70 lang=cpp
+ *
+ * [70] Climbing Stairs
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        if (n == 1 or n == 0)
+            return 1;
+        int a = 1, b = 1, c;
+        for (int i = 2; i <= n; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+};
+// @lc code=end
