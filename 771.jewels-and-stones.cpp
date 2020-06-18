6f2aa5a506359ee02b7d7db7643cada8fcf94ee1
Abhimanyu Maurya <aerma7309@gmail.com>
2020-06-19 01:07:32 +0530

    771.jewels-and-stones.cpp

--- 771.jewels-and-stones.cpp
+++ 771.jewels-and-stones.cpp
@@ -5,15 +5,25 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int numJewelsInStones(string J, string S)
+    // int numJewelsInStones(string J, string S)
+    // {
+    //     int cnt = 0;
+    //     for (char ch : J)
+    //         cnt += count(S.begin(), S.end(), ch);
+    //     return cnt;
+    // }
+
+    int numJewelsInStones(string j, string s)
     {
         int cnt = 0;
-        for (char ch : J)
-            cnt += count(S.begin(), S.end(), ch);
+        unordered_set<char> m(j.begin(), j.end());
+        for (auto &i : s)
+            if (m.find(i) != m.end())
+                cnt++;
         return cnt;
     }
 };
 // @lc code=end
-
