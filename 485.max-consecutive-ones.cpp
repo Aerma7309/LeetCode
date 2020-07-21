ae1c6c8b4079c089e2002102b13820d43da9c708
Abhimanyu Maurya <aerma7309@gmail.com>
2020-07-21 12:57:43 +0530

    485.max-consecutive-ones.cpp

--- 485.max-consecutive-ones.cpp
+++ 485.max-consecutive-ones.cpp
@@ -5,21 +5,34 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
+    // int findMaxConsecutiveOnes(vector<int> &nums)
+    // {
+    //     int i = -1, j = 0, size = nums.size(), res = 0;
+    //     while (i < size and j < size)
+    //     {
+    //         if (nums[j])
+    //             res = max(res, j - i);
+    //         else
+    //             i = j;
+    //         ++j;
+    //     }
+    //     return res;
+    // }
+
     int findMaxConsecutiveOnes(vector<int> &nums)
     {
-        int i = -1, j = 0, size = nums.size(), res = 0;
-        while (i < size and j < size)
+        int res = 0;
+        for (int i = 0, j = -1; i < nums.size(); i++)
         {
-            if (nums[j])
-                res = max(res, j - i);
+            if (nums[i] == 1)
+                res = max(res, i - j);
             else
-                i = j;
-            ++j;
+                j = i;
         }
         return res;
     }
 };
 // @lc code=end
-
