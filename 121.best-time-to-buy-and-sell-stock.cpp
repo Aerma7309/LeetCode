9f7a8d30867a52a61e453d0f468f9c344a941a81
Abhimanyu Maurya <aerma7309@gmail.com>
2021-06-28 19:20:54 +0530

    121.best-time-to-buy-and-sell-stock.cpp

--- 121.best-time-to-buy-and-sell-stock.cpp
+++ 121.best-time-to-buy-and-sell-stock.cpp
@@ -9,11 +9,19 @@ using namespace std;
 class Solution
 {
 public:
+    // int maxProfit(vector<int> &prices)
+    // {
+    //     int mx = prices.back(), profit = 0;
+    //     for (int i = prices.size() - 2; i >= 0; i--)
+    //         profit = max(profit, mx - prices[i]), mx = max(mx, prices[i]);
+    //     return profit;
+    // }
+
     int maxProfit(vector<int> &prices)
     {
-        int mx = prices.back(), profit = 0;
-        for (int i = prices.size() - 2; i >= 0; i--)
-            profit = max(profit, mx - prices[i]), mx = max(mx, prices[i]);
+        int mn = prices.front(), profit = 0;
+        for (int i = 1; i < prices.size(); i++)
+            profit = max(prices[i] - mn, profit), mn = min(mn, prices[i]);
         return profit;
     }
 };
