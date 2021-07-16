4bf712effc7fa1ec01bf8785a3b8370d972ee8f6
Abhimanyu Maurya <aerma7309@gmail.com>
2021-07-16 18:05:01 +0530

    27.remove-element.cpp

--- 27.remove-element.cpp
+++ 27.remove-element.cpp
@@ -3,25 +3,36 @@
  *
  * [27] Remove Element
  */
-
+#include <bits/stdc++.h>
+using namespace std;
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
     int removeElement(vector<int> &nums, int val)
     {
-        int s = nums.size();
-        int c = count(nums.begin(), nums.end(), val);
-        for (int i = 0, j = s - 1; i < s - c; i++)
+        // int s = nums.size();
+        // int c = count(nums.begin(), nums.end(), val);
+        // for (int i = 0, j = s - 1; i < s - c; i++)
+        // {
+        //     if (nums[i] == val)
+        //     {
+        //         while (nums[j] == val)
+        //             j--;
+        //         swap(nums[i], nums[j]);
+        //     }
+        // }
+        // return s - c;
+
+        int n = nums.size(), i, j;
+        for (i = 0, j = 0; j < n; j++)
         {
-            if (nums[i] == val)
-            {
-                while (nums[j] == val)
-                    j--;
-                swap(nums[i], nums[j]);
-            }
+            if (nums[j] == val)
+                continue;
+            nums[i] = nums[j];
+            i++;
         }
-        return s - c;
+        return i;
     }
 };
 // @lc code=end
-
