206cbb20acbe853fa36e48db393f0a996adc9066
Abhimanyu Maurya <aerma7309@gmail.com>
2021-07-16 12:17:16 +0530

    387.first-unique-character-in-a-string.cpp

--- 387.first-unique-character-in-a-string.cpp
+++ 387.first-unique-character-in-a-string.cpp
@@ -3,37 +3,45 @@
  *
  * [387] First Unique Character in a String
  */
-
+#include <bits/stdc++.h>
+using namespace std;
 // @lc code=start
 class Solution
 {
 public:
     int firstUniqChar(string s)
     {
-        list<char> l;
-        map<char, list<char>::iterator> m;
-        map<char, bool> f;
-        for (char &i : s)
-        {
-            if (f.find(i) != f.end())
-            {
-                if (m.find(i) != m.end())
-                {
-                    l.erase(m[i]);
-                    m.erase(i);
-                }
-            }
-            else
-            {
-                l.push_back(i);
-                m[i] = --l.end();
-                f[i] = true;
-            }
-        }
-        if (l.empty())
-            return -1;
-        else
-            return s.find(l.front());
+        // list<char> l;
+        // map<char, list<char>::iterator> m;
+        // map<char, bool> f;
+        // for (char &i : s)
+        // {
+        //     if (f.find(i) != f.end())
+        //     {
+        //         if (m.find(i) != m.end())
+        //         {
+        //             l.erase(m[i]);
+        //             m.erase(i);
+        //         }
+        //     }
+        //     else
+        //     {
+        //         l.push_back(i);
+        //         m[i] = --l.end();
+        //         f[i] = true;
+        //     }
+        // }
+        // if (l.empty())
+        //     return -1;
+        // else
+        //     return s.find(l.front());
+        map<char, int> m;
+        for (auto &ch : s)
+            m[ch]++;
+        for (int i = 0; i < s.size(); i++)
+            if (m[s[i]] == 1)
+                return i;
+        return -1;
     }
 };
 // @lc code=end
