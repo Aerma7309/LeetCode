498f160936013bde48fcc0851ca6c186e8b886d9
Abhimanyu Maurya <aerma7309@gmail.com>
2021-07-16 12:45:08 +0530

    387.first-unique-character-in-a-string.cpp

--- 387.first-unique-character-in-a-string.cpp
+++ 387.first-unique-character-in-a-string.cpp
@@ -1,3 +1,11 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem387.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
 /*
  * @lc app=leetcode id=387 lang=cpp
  *
@@ -11,6 +19,8 @@ class Solution
 public:
     int firstUniqChar(string s)
     {
+        // using something like LRU cache making list of
+        // contestant and using map to access in constant time
         // list<char> l;
         // map<char, list<char>::iterator> m;
         // map<char, bool> f;
@@ -35,13 +45,31 @@ public:
         //     return -1;
         // else
         //     return s.find(l.front());
+
+        // using map and two traversal of string
+        // map<char, int> m;
+        // for (auto &ch : s)
+        //     m[ch]++;
+        // for (int i = 0; i < s.size(); i++)
+        //     if (m[s[i]] == 1)
+        //         return i;
+        // return -1;
+
+        // using map and invalidation trick
+        // if char repeats set index as sizeof input string
         map<char, int> m;
-        for (auto &ch : s)
-            m[ch]++;
         for (int i = 0; i < s.size(); i++)
-            if (m[s[i]] == 1)
-                return i;
-        return -1;
+        {
+            char ch = s[i];
+            if (m.find(ch) == m.end())
+                m[ch] = i;
+            else
+                m[ch] = s.size();
+        }
+        int minIndex = s.size();
+        for (auto &it : m)
+            minIndex = min(minIndex, it.second);
+        return minIndex == s.size() ? -1 : minIndex;
     }
 };
 // @lc code=end
