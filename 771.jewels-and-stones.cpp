fddd8adb600ad87a9bd19783bb8831b39e536f42
Abhimanyu Maurya <aerma7309@gmail.com>
2021-07-17 18:42:50 +0530

    771.jewels-and-stones.cpp

--- 771.jewels-and-stones.cpp
+++ 771.jewels-and-stones.cpp
@@ -3,7 +3,8 @@
  *
  * [771] Jewels and Stones
  */
-
+#include <bits/stdc++.h>
+using namespace std;
 // @lc code=start
 class Solution
 {
@@ -16,13 +17,25 @@ public:
     //     return cnt;
     // }
 
+    // int numJewelsInStones(string j, string s)
+    // {
+    //     int cnt = 0;
+    //     unordered_set<char> m(j.begin(), j.end());
+    //     for (auto &i : s)
+    //         if (m.find(i) != m.end())
+    //             cnt++;
+    //     return cnt;
+    // }
+
     int numJewelsInStones(string j, string s)
     {
+        bitset<256> m;
         int cnt = 0;
-        unordered_set<char> m(j.begin(), j.end());
-        for (auto &i : s)
-            if (m.find(i) != m.end())
-                cnt++;
+        for (auto &ch : j)
+            m[(int)ch] = true;
+        for (auto &ch : s)
+            if (m[(int)ch])
+                ++cnt;
         return cnt;
     }
 };
