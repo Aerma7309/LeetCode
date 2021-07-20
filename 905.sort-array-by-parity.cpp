858b568a93d0c191dbb6d16482f4a282d13c39ef
Abhimanyu Maurya <aerma7309@gmail.com>
2021-07-20 19:35:02 +0530

    905.sort-array-by-parity.cpp

--- 905.sort-array-by-parity.cpp
+++ 905.sort-array-by-parity.cpp
@@ -3,21 +3,31 @@
  *
  * [905] Sort Array By Parity
  */
-
+#include <bits/stdc++.h>
+using namespace std;
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    vector<int> sortArrayByParity(vector<int>& A) {
-        auto l = A.begin();
-        auto r = --A.end();
-        while (distance(l,r)>0)
-        {
-            if(*l%2==1 and *r%2==0) swap(*l,*r);
-            else if(*l%2==0) l++;
-            else r--;
-        }   
+    // vector<int> sortArrayByParity(vector<int>& A) {
+    //     auto l = A.begin();
+    //     auto r = --A.end();
+    //     while (distance(l,r)>0)
+    //     {
+    //         if(*l%2==1 and *r%2==0) swap(*l,*r);
+    //         else if(*l%2==0) l++;
+    //         else r--;
+    //     }
+    //     return A;
+    // }
+
+    vector<int> sortArrayByParity(vector<int> &A)
+    {
+        int idx = -1;
+        for (int i = 0; i < A.size(); i++)
+            if (not(A[i] & 1))
+                swap(A[++idx], A[i]);
         return A;
     }
 };
 // @lc code=end
-
