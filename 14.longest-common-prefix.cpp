c2be1fff48b4f27d577242b763106f409f20e96e
Abhimanyu Maurya <aerma7309@gmail.com>
2021-07-22 05:49:45 +0530

    14.longest-common-prefix.cpp

--- 14.longest-common-prefix.cpp
+++ 14.longest-common-prefix.cpp
@@ -4,88 +4,52 @@
  * [14] Longest Common Prefix
  */
 #include <bits/stdc++.h>
+using namespace std;
 // @lc code=start
+
+class Trie
+{
+private:
+    bool isTerminal;
+    map<char, Trie *> nodes;
+
+public:
+    Trie() { isTerminal = false; };
+    ~Trie(){};
+    void insert(string word)
+    {
+        Trie *temp = this;
+        for (auto &ch : word)
+        {
+            if (temp->nodes.find(ch) == temp->nodes.end())
+                temp->nodes[ch] = new Trie();
+            temp = temp->nodes[ch];
+        }
+        temp->isTerminal = true;
+    }
+    string LongestCommonPrefix()
+    {
+        Trie *temp = this;
+        string ans = "";
+        while ((temp->nodes.size() == 1) and not temp->isTerminal)
+        {
+            ans.push_back(temp->nodes.begin()->first);
+            temp = temp->nodes[temp->nodes.begin()->first];
+        }
+        return ans;
+    }
+};
+
 class Solution
 {
-    class Trie
-    {
-        bool isTerminal;
-        map<char, Trie *> h;
-
-    public:
-        /** Initialize your data structure here. */
-        Trie()
-        {
-            isTerminal = false;
-        }
-
-        /** Inserts a word into the trie. */
-        void insert(string word)
-        {
-            Trie *curr = this;
-            for (auto &i : word)
-            {
-                if (curr->h.find(i) != curr->h.end())
-                    curr = curr->h[i];
-                else
-                {
-                    Trie *newNode = new Trie();
-                    curr->h[i] = newNode;
-                    curr = newNode;
-                }
-            }
-            curr->isTerminal = true;
-        }
-
-        /** Returns if the word is in the trie. */
-        bool search(string word)
-        {
-            Trie *curr = this;
-            for (auto &i : word)
-            {
-                if (curr->h.find(i) != curr->h.end())
-                    curr = curr->h[i];
-                else
-                    return false;
-            }
-            return curr->isTerminal;
-        }
-
-        /** Returns if there is any word in the trie that starts with the given prefix. */
-        bool startsWith(string prefix)
-        {
-            Trie *curr = this;
-            for (auto &i : prefix)
-            {
-                if (curr->h.find(i) != curr->h.end())
-                    curr = curr->h[i];
-                else
-                    return false;
-            }
-            return true;
-        }
-
-        string longestPrefix()
-        {
-            Trie *curr = this;
-            string str = "";
-            while (curr->h.size() == 1 and !curr->isTerminal)
-            {
-                char c = curr->h.begin()->first;
-                str = str + c;
-                curr = curr->h[c];
-            }
-            return str;
-        }
-    };
 
 public:
     string longestCommonPrefix(vector<string> &strs)
     {
-        Trie *sol = new Trie;
-        for (auto &i : strs)
-            sol->insert(i);
-        return sol->longestPrefix();
+        Trie trie;
+        for (auto &word : strs)
+            trie.insert(word);
+        return trie.LongestCommonPrefix();
     }
 };
 // @lc code=end
