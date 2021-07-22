bfb395c220bc25d5a527c027b6f2c8349a5df1c3
Abhimanyu Maurya <aerma7309@gmail.com>
2021-07-22 06:25:45 +0530

    64.minimum-path-sum.cpp

--- 64.minimum-path-sum.cpp
+++ 64.minimum-path-sum.cpp
@@ -3,26 +3,23 @@
  *
  * [64] Minimum Path Sum
  */
-
+#include <bits/stdc++.h>
+using namespace std;
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int minPathSum(vector<vector<int>> &grid)
+    int minPathSum(vector<vector<int>> grid)
     {
-        if (grid.size() == 0)
-            return 0;
         int r = grid.size(), c = grid[0].size();
-        vector<vector<int>> path(r, vector<int>(c));
-        path[0][0] = grid[0][0];
         for (int i = 1; i < r; i++)
-            path[i][0] = path[i - 1][0] + grid[i][0];
+            grid[i][0] += grid[i - 1][0];
         for (int i = 1; i < c; i++)
-            path[0][i] = path[0][i - 1] + grid[0][i];
+            grid[0][i] += grid[0][i - 1];
         for (int i = 1; i < r; i++)
             for (int j = 1; j < c; j++)
-                path[i][j] = min(path[i - 1][j], path[i][j - 1]) + grid[i][j];
-        return path[r - 1][c - 1];
+                grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]);
+        return grid[r - 1][c - 1];
     }
 };
 // @lc code=end
-
