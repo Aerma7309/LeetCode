8811a6969e099a0cb32d1451749956f01e9dfb99
Abhimanyu Maurya <aerma7309@gmail.com>
2021-07-29 10:12:46 +0530

    355.design-twitter.cpp

--- 355.design-twitter.cpp
+++ 355.design-twitter.cpp
@@ -1,11 +1,3 @@
-// @before-stub-for-debug-begin
-#include <vector>
-#include <string>
-#include "commoncppproblem355.h"
-
-using namespace std;
-// @before-stub-for-debug-end
-
 /*
  * @lc app=leetcode id=355 lang=cpp
  *
